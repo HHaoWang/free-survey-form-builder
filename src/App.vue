2c107d27f731670484ab26fbc14fa45d3d129f30
HHao <work@hhao.wang>
2023-09-14 17:29:15 +0800

    - 修复示例中属性缺失的问题

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <div style='height: 100vh'>
-    <free-survey-form-builder></free-survey-form-builder>
+  <div style="height: 100vh">
+    <free-survey-form-builder v-model:survey="survey"></free-survey-form-builder>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { Survey } from 'free-survey-core';
+
+const survey = ref(new Survey());
+</script>
 
 <style lang="less">
 body {
   padding: 0;
-  margin: 0 ;
+  margin: 0;
 }
 </style>
